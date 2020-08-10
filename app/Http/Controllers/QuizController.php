cc84b64d497fe987a17ac5f904aae8d507d2aecf
rooberthh <roberth.evaldsson@hotmail.com>
2020-08-10 18:08:45 +0200

    scaffold ui via laravel-ui

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Question;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
+    public function index()
+    {
+        $questions = Question::all()->paginate(3);
+
+        return view('quiz.index', ['questions' => $questions]);
+    }
 
 }
