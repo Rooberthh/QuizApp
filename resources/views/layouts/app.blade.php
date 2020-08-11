8c41b0f1a2cce8280dfb36f1ad8081da39c64c72
rooberthh <roberth.evaldsson@hotmail.com>
2020-08-11 18:31:55 +0200

    add navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    @include('layouts.nav')
     <div id="app">
         <main class="py-4">
             @yield('content')
